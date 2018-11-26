1f845f6d1e359f6c7fb42bec865647dd501f39ec
seungjunn <seungjunn@etri.re.kr>
2018-11-26 14:25:29 +0900

    Add test codes for gettimeofday in application folder

---
+++ application/gettimeofday_test/gettimeofday_test.c
@@ -0,0 +1,24 @@
+#include "utility.h"
+#include "syscall.h"
+
+int main()
+{
+  struct az_timeval tv1,tv2 ;
+
+  print_xy(0,0, "HELLO") ;
+ 
+  az_gettimeofday(&tv1, NULL) ;
+
+  print_xy(0,1, "HELLO2") ;
+
+  delay(1) ;  
+ 
+  print_xy(0,2,"HELLO3") ;
+
+  az_gettimeofday(&tv2, NULL) ;
+
+  print_xy(0,3, "sec %q , msec %q ", tv2.tv_sec, tv2.tv_usec ) ;
+  print_xy(0,4, "sec %q , msec %q ", tv1.tv_sec, tv1.tv_usec ) ;
+
+  return 0;
+}
